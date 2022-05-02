a36dbe6233a36fd8a2bfb41dbd8415ed47870ffd
Joo An Choi <jooanchoi@gmail.com>
2022-05-02 18:36:27 -0400

    Reset Levels for both players

--- Assets/GameEnd.cs
+++ Assets/GameEnd.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameEnd : MonoBehaviour
 {
+    public bool isPlayer1;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +20,13 @@ public class GameEnd : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         other.GetComponent<Ball>().InitiateGame();
+        if (isPlayer1)
+        {
+            FindObjectOfType<BrickManagerPlayer1>().ResetLevel();
+        }
+        else
+        {
+            FindObjectOfType<BrickManagerPlayer2>().ResetLevel();
+        }
     }
 }

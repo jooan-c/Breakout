5a18d49e7ad71e08a9d1f68a313a8146d3403857
GVux <62868382+GVux@users.noreply.github.com>
2022-05-28 22:41:33 -0700

    Add files via upload
    Refactor

--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -36,7 +36,6 @@ public class Paddle : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
 
-        //citation: https://www.youtube.com/watch?v=RYG8UExRkhA
         Ball ball = collision.gameObject.GetComponent<Ball>();
        
         if (ball != null)

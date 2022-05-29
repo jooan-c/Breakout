03428140476c6eca2bd688c3fc13d59634b30e0f
GVux <62868382+GVux@users.noreply.github.com>
2022-05-28 18:05:02 -0700

    Add files via upload
    Added code to change ball's angle and cleaned up code

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static System.Random;
 
 public class Ball : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D rb;
     public bool isPlayer1;
-    
+    private float collisionPaddleSide;
+    float randVariance = .15f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +21,7 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void InitiateGame()
@@ -30,8 +34,19 @@ public class Ball : MonoBehaviour
         {
             transform.position = new Vector2(-5, -2);
         }
-        float x = 0.75F;
+        float x = .20F;
         float y = -1;
-        rb.velocity = new Vector2(speed * x, speed * y);
+        float startX = NewFloat(-randVariance, randVariance);
+        float startY = NewFloat(-randVariance, randVariance);
+        rb.velocity = new Vector2(speed * (x + startX), speed * (y + startY));
+    }
+
+
+    // citation: https://www.codegrepper.com/code-examples/csharp/c%23+random+float+between+two+numbers
+    static float NewFloat(float minVar, float maxVar)
+    {
+        System.Random rand = new System.Random();
+        double val = (rand.NextDouble() * (maxVar - minVar) + minVar);
+        return (float)val;
     }
 }
--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static System.Math;
+
 
 public class Paddle : MonoBehaviour
 {
@@ -8,7 +10,13 @@ public class Paddle : MonoBehaviour
     public float speed;
     public Rigidbody2D rb;
 
+    public float maxBounceAngl = 55f;
     private float movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
 
     // Update is called once per frame
     void Update()
@@ -23,5 +31,27 @@ public class Paddle : MonoBehaviour
         }
 
         rb.velocity = new Vector2(movement * speed, rb.velocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        //citation: https://www.youtube.com/watch?v=RYG8UExRkhA
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+       
+        if (ball != null)
+        {
+            float offsetDist = transform.position.x - collision.GetContact(0).point.x;
+            float wdth = collision.otherCollider.bounds.size.x / 2;
+            float currAngle = Vector2.SignedAngle(Vector2.up, ball.GetComponent<Rigidbody2D>().velocity);
+            float bounceAng = (offsetDist / wdth) * maxBounceAngl;
+            float newAngle = Mathf.Clamp(currAngle + bounceAng, -maxBounceAngl, maxBounceAngl);
+
+            Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
+            ball.GetComponent<Rigidbody2D>().velocity = rotation * Vector2.up * ball.GetComponent<Rigidbody2D>().velocity.magnitude;
+
+
+        }
+
     }
 }
